70b6d1f748a2caca4ab1a5c7a963ec09ff75ad6a
jorgeslfreitas <jorgeslfreitas@gmail.com>
2023-06-23 20:58:33 -0300

    Exercício 8 Condicional - Resolvido

---
+++ Secao03/Exercicios_For/Exercicio_Condicional_08/Exercicio_Condicional_08/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Condicional_08
+{
+    class Exercicio_08
+    {
+        static void Main(string[] args)
+        {
+            double imposto;
+
+            Console.Write("Digite seu salário: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 2000.00)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if (salario <= 3000.00)
+            {
+                imposto = salario * 0.08;
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario <= 4500.00)
+            {
+                imposto = 1000 * 0.08;
+                imposto = imposto + (salario - 3000.00) * 0.18;
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                imposto = 1000 * 0.08;
+                imposto = imposto + 1500 * 0.18;
+                imposto = imposto + (salario - 4500.00) * 0.28;
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
