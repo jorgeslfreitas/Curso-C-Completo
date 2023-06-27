8af17b29e1bce70e67e175164bb8e4da10213381
jorgeslfreitas <jorgeslfreitas@gmail.com>
2023-06-26 22:18:32 -0300

    Seção 4 - exercícios de fixação1 e 2

---
+++ Secao04/Exercicios/Exercicio_Fixacao_Classe_01/Exercicio_Fixacao_Classe_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_Fixacao_Classe_01
+{
+    class Exercicio_Fixacao_01
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1, pessoa2;
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            Console.Write("Digite nome e idade da primeira pessoa: ");
+            string[] vetPessoa1 = Console.ReadLine().Split(" ");
+            Console.Write("Digite nome e idade da segunda pessoa: ");
+            string[] vetPessoa2 = Console.ReadLine().Split(" ");
+
+            pessoa1.Nome = vetPessoa1[0];
+            pessoa1.Idade = int.Parse(vetPessoa1[1]);
+
+            pessoa2.Nome = vetPessoa2[0];
+            pessoa2.Idade = int.Parse(vetPessoa2[1]);
+
+            if(pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha {pessoa1.Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoa2.Nome}");
+            }
+        }
+    }
+}
---
+++ Secao04/Exercicios/Exercicio_Fixacao_Classe_02/Exercicio_Fixacao_Classe_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Classe_02
+{
+    class Exercicio_Fixacao_02
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func1, func2;
+            func1 = new Funcionario();
+            func2 = new Funcionario();
+
+            double salarioMedio;
+
+            Console.Write("Digite o nome e salário do primeiro funcinário: ");
+            string[] vetFunc1 = Console.ReadLine().Split(" ");
+            Console.Write("Digite o nome e salário do segundo funcionário: ");
+            string[] vetFunc2 = Console.ReadLine().Split(" ");
+
+            func1.Nome = vetFunc1[0];
+            func1.Salario = double.Parse(vetFunc1[1], CultureInfo.InvariantCulture);
+
+            func2.Nome = vetFunc2[0];
+            func2.Salario = double.Parse(vetFunc2[1], CultureInfo.InvariantCulture);
+
+            salarioMedio = (func1.Salario + func2.Salario) / 2;
+
+            Console.Write("Salário médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
